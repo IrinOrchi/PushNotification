6f5b48a336d4b3808af19ff51e9c1a9738291ce6
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2026-04-15 17:39:09 +0600

    project architecture

--- push-notification/src/app/app.routes.ts
+++ push-notification/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PushNotificationsComponent } from './pages/push-notifications/push-notifications.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: '',
+    redirectTo: 'PushNotifications',
+    pathMatch: 'full'
+  },
+  {
+    path: 'PushNotifications',
+    component: PushNotificationsComponent,
+  },
+];
---
+++ push-notification/src/app/pages/push-notifications/push-notifications.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { PushNotificationService } from '../../services/push-notification.service';
+
+@Component({
+  selector: 'app-push-notifications',
+  imports: [],
+  templateUrl: './push-notifications.component.html',
+  styleUrl: './push-notifications.component.scss'
+})
+export class PushNotificationsComponent {
+   protected readonly title = signal('Push Notification Demo');
+  protected readonly notificationService = inject(PushNotificationService);
+  protected readonly permission = this.notificationService.permission;
+  protected readonly isSupported = this.notificationService.isSupported();
+  protected readonly statusMessage = computed(() => {
+    if (!this.isSupported) {
+      return 'Notifications are not supported in this browser.';
+    }
+
+    return `Current permission: ${this.permission()}`;
+  });
+
+  protected async enableNotifications(): Promise<void> {
+    await this.notificationService.requestPermission();
+  }
+
+  protected sendTestNotification(): void {
+    this.notificationService.showNotification(
+      'Push Notification',
+      'Notification content test By Irin Hoque Orchi.'
+    );
+  }
+
+}
