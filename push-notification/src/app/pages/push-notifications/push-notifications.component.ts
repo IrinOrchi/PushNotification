7f9a53b1a70efa14c8a433dd51839517eea9b020
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2026-04-18 14:32:25 +0600

    removed

--- push-notification/src/app/pages/push-notifications/push-notifications.component.ts
+++ push-notification/src/app/pages/push-notifications/push-notifications.component.ts
@@ -8,27 +8,6 @@ import { PushNotificationService } from '../../services/push-notification.servic
   styleUrl: './push-notifications.component.scss'
 })
 export class PushNotificationsComponent {
-   protected readonly title = signal('Push Notification Demo');
-  protected readonly notificationService = inject(PushNotificationService);
-  protected readonly permission = this.notificationService.permission;
-  protected readonly isSupported = this.notificationService.isSupported();
-  protected readonly statusMessage = computed(() => {
-    if (!this.isSupported) {
-      return 'Notifications are not supported in this browser.';
-    }
-
-    return `Current permission: ${this.permission()}`;
-  });
-
-  protected async enableNotifications(): Promise<void> {
-    await this.notificationService.requestPermission();
-  }
-
-  protected sendTestNotification(): void {
-    this.notificationService.showNotification(
-      'Push Notification',
-      'Notification content test By Irin Hoque Orchi.'
-    );
-  }
+ 
 
 }
